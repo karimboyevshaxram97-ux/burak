e10ad086b175733781ebf9b1fc03a4e396ff8193
KEVIN <karimboyevshaxram97@gmail.com>
2026-01-13 13:42:25 +0900

    feat: burak loyixasi va task bajarildi

---
+++ src/train.ts
@@ -0,0 +1,20 @@
+//G-TASK
+//Shunday function tuzingki unga integerlardan iborat array pass 
+ //bolsin va function bizga osha arrayning eng katta qiymatiga
+ //tegishli birinchi indexni qaytarsin. 
+ //MASALAN: getHighestIndex([5, 21, 12, 21, 8]) return qiladi
+ // 1 sonini.
+
+ function getHighestIndex(arr: number[]): number {
+  let max = arr[0];
+  let index = 0;
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+      index = i;
+    }
+  }
+
+  return index;
+}
