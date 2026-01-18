caf6571a6dc38bf9eb660394e56d25e132075309
KEVIN <karimboyevshaxram97@gmail.com>
2026-01-18 18:26:04 +0900

    fix: xato tuzatildi

--- src/views/router.ts
+++ src/views/router.ts
@@ -1,11 +1,11 @@
 import express from "express";
 const router = express.Router();
  import memberController from "../controllers/member.controllers";
-
+/*
 router.get("/", memberController.goHome);
 
  router.get("/Login", memberController.getLogin);
 
  router.get("/Signup", memberController.getSignup);
-
+*/
 export default router;
