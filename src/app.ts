d9693ab87e8d3935f021c7cfd2735bf548559a41
KEVIN <karimboyevshaxram97@gmail.com>
2026-01-20 21:19:32 +0900

    fix: xato togirlandi

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,21 @@
 import express from "express";
 import path from "path";
-import router from "./views/router"
+import router from "./views/router";
 import routerAdmin from "./views/router-admin";
 import morgan from "morgan";
+import { MORGAN_FORMAT } from "./libs/types/config";
 
-/** 1-ENTRANCE kirish kodlari**/
 const app = express();
+
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(morgan(`MORGAN_FORMAT`));
+app.use(morgan(MORGAN_FORMAT));
 
-/** 2-SESSIONS **/
-
-/** 3-VIEWS **/
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-/** 4-ROUTERS **/
-app.use("/admin",routerAdmin);     // SSR: backendda frontendni qurib oliosh va biz bunda EJS framworkdan foydalanamiz
-app.use("/", router);             // SPA: REACT ( bizning 1-maqsadimiz burak backand serverni loyixamizga react  spa sifatida ishlatamiz)
+app.use("/admin", routerAdmin);
+app.use("/", router);
 
 export default app;
