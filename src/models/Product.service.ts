ae305a7cd71a9c5c02d3e48cc7a6236a02be597e
KEVIN <karimboyevshaxram97@gmail.com>
2026-02-01 04:32:14 +0900

    feat: configure product Schema model

--- src/models/Product.service.ts
+++ src/models/Product.service.ts
@@ -1,5 +1,11 @@
+import ProductModel from "../schema/Product.models";
+
 class ProductService {
-    
+  private readonly productModel;
+
+  constructor() {
+    this.productModel = ProductModel;
+  }
 }
 
 export default ProductService;
