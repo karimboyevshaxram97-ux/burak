22fcf9218c54426f24cd0e19b8495f928d3518e6
KEVIN <karimboyevshaxram97@gmail.com>
2026-02-08 16:41:55 +0900

    feat: rasm uzgarish va task ishladi

--- src/train.ts
+++ src/train.ts
@@ -1,3 +1,25 @@
+
+/*
+Q-TASK
+
+Shunday function yozing, u 2 ta parametrgga ega bolib birinchisi object,
+ ikkinchisi string. Agar string parametr objectni propertysi bolsa true 
+ bolmasa false qaytarsin. MASALAN: hasProperty({name: "BMW", model: "M3"}, "model") 
+ return true; hasProperty({name: "BMW", model: "M3"}, "year") return false.
+*/
+
+function hasProperty(obj: Record<string, unknown>, prop: string): boolean {
+  return obj.hasOwnProperty(prop);
+}
+
+
+console.log(hasProperty({name: "BMW", model: "M3"}, "model")); 
+console.log(hasProperty({name: "BMW", model: "M3"}, "year"));  
+
+//=======================================================================
+/*
+
+
 function objectToArray(obj: Record<string, any>): [string, any][] {
     return Object.entries(obj);
 }
@@ -158,7 +180,8 @@ console.log(findLongestWord("I come from Uzbekistan")); // Natija: "Uzbekistan"
    - css: snake_case
    -error handling
 */
- /**
+
+ /*
   traditional api
   rest api
   graphql api
