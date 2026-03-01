c1163fc573922d146b4df681c3d9abdf60ed6c3e
KEVIN <karimboyevshaxram97@gmail.com>
2026-03-01 17:54:33 +0900

    fix: tasck done

--- src/train.ts
+++ src/train.ts
@@ -1,10 +1,28 @@
+/**
+ * Z-TASK
+Shunday function yozing, uni sonlardan tashkil topgan array qabul qilsin.
+ Function arraydagi juft sonlarni yigindisini qaytarsin. 
+ MASALAN: sumEvens([1,2,3]) return 2.
+
+ */
+
+function sumEvens(arr: number[]): number {
+  return arr
+    .filter(num => num % 2 === 0) 
+    .reduce((sum, num) => sum + num, 0); 
+}
+
+console.log(sumEvens([1, 2, 3])); 
+console.log(sumEvens([4, 5, 6, 7, 8])); 
+
+//====================================================================
 /*
 Y-TASK
 
 Shunday function yozing, uni 2 ta array parapetri bolsin. 
 Function ikkala arrayda ham ishtirok etgan qiymatlarni bir arrayda 
 qaytarsin. MASALAN: findIntersection([1,2,3], [3,2,0]) return [2,3].
-*/
+
 
 function findIntersection<T>(arr1: T[], arr2: T[]): T[] {
   return arr1.filter(value => arr2.includes(value));
@@ -12,7 +30,7 @@ function findIntersection<T>(arr1: T[], arr2: T[]): T[] {
 
 // Misol:
 console.log(findIntersection([1, 2, 3], [3, 2, 0])); 
-
+*/
 //=======================================================================
 /**X-TASK
 
