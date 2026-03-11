68bf289426d8a4b330062fcd50b2f008f495020f
KEVIN <karimboyevshaxram97@gmail.com>
2026-03-12 03:41:41 +0900

    feat: ZD task done

--- src/train.ts
+++ src/train.ts
@@ -1,10 +1,32 @@
+/**ZD-TASK
+
+Shunday function yozing, uni number, array va number parametrlari
+ bolsin va berilgan 1-parametr numberga teng indexni array ichidan 
+ topib 3-parametrdagi raqam bilan almashtirib yangilangan arrayni qaytarsin.
+  MASALAN: changeNumberInArray(1, [1,3,7,2], 2) return [1,2,7,2]. */
+
+function changeNumberInArray(target: number, arr: number[], newValue: number): number[] {
+  const updatedArr = [...arr];
+  const index = updatedArr.indexOf(target);
+  if (index !== -1) {
+    updatedArr[index] = newValue;
+  }
+
+  return updatedArr;
+}
+
+
+console.log(changeNumberInArray(1, [1, 3, 7, 2], 2)); 
+
+//========================================================================
+
 /*
 ZC-TASK
 
 Shunday function yozing, uni number parametri bolsin va function qabul 
 parametrni selsiy miqdori sifatida qabul qilib uni farenhitga ozgartirib bersin.
  MASALAN: celsiusToFahrenheit(0) return 32.
-*/
+
 
 function celsiusToFahrenheit(celsius: number): number {
   return (celsius * 9/5) + 32;
@@ -14,7 +36,7 @@ function celsiusToFahrenheit(celsius: number): number {
 console.log(celsiusToFahrenheit(0));    
 console.log(celsiusToFahrenheit(100));  
 console.log(celsiusToFahrenheit(37));   
-
+*/
 //====================================================================
 /**ZB-TASK
 
